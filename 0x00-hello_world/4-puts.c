db7da4ffa501aed47cbb537de5a732dab3dc3b70
godfrey9805 <godfrey9805@gmail.com>
2023-06-15 03:13:53 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * main program printing a line with put function 
+ * Return:0
+ */
+int main(void){
+	put("\"Programming is like building a multilingual puzzle)
+		return (0)
+}
